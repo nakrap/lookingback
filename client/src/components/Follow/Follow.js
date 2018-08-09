db9f248c8fc6b5e38b5cdc6a74267bcde1782d0b
Ken <khuynh024@gmail.com>
2018-08-08 20:35:36 -0500

    Dashboard page working

--- client/src/components/Follow/Follow.js
+++ client/src/components/Follow/Follow.js
@@ -1,10 +1,68 @@
 import React from "react";
 import "./Follow.css"
+import Cards from "../Cards"
+import friends from "./friends.json"
 
-const Follow = () => (
-<div>
+class Follow extends React.Component {
+  state = {
+    showFollowers: true,
+    friends: friends
+  }
 
-  </div>
-);
+  following = (e)=>{
+    this.setState({
+      showFollowers: true
+    })
+    console.log(this.state.showFollowers)
+  }
+
+  toFollow = (e)=>{
+    this.setState({
+      showFollowers: false
+    })
+    console.log(this.state.showFollowers)
+  }
+
+  render(){
+    const showFollowers = this.state.showFollowers;
+    return(
+      <div className="ui container center aligned">
+        <h1><a onClick={this.following}>Following</a> | <a onClick={this.toFollow}>People to Follow</a></h1>
+
+        {showFollowers ? (
+          <div>
+            <h1>People you are Following</h1> 
+            
+            {this.state.friends.map(friend => (
+            <Cards
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}            
+            />
+          ))}
+          </div>   
+      ) : (
+        <div>
+        <h1>People to Follow</h1> 
+        
+        {this.state.friends.map(friend => (
+        <Cards
+        id={friend.id}
+        key={friend.id}
+        // name={friend.name}
+        image={friend.image}            
+        />
+      ))}
+      </div>
+        )}
+
+      </div>
+
+    )
+
+  }
+}
+
 
 export default Follow;
--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Welcome.css"
 
-const Welcome = props => (
-<div>
-  Welcome {props.name} 
-  </div>
-);
+class Welcome extends React.Component {
+  state = {
+    display: true
+  }
+
+  closeMessage = (e) =>{
+    this.setState({
+      display: false
+    })
+    console.log(this.state.display);
+    }
+  
+  render() {
+    const display = this.state.display;
+
+    if (display === true){
+      return(
+        <div className="welcomeMessage">
+          <div className="ui yellow message ">
+            <i className="close icon" onClick={this.closeMessage}></i>
+            <div className="header ">
+            Welcome back {this.props.name}!!!
+            </div>
+            <p>What are you up?</p>
+          </div>
+        </div>
+      )
+    } else{
+      return(
+        <div>
+
+        </div>
+      )
+    }
+    
+  }
+}
+
 
 export default Welcome;
