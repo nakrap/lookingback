7b0e9bcbcaee9f4f0876b57cb9fdfd4fe9ba91bd
theRealGerph <jvongkoth@gmail.com>
2018-08-23 18:14:25 -0500

    updated components

--- client/src/components/ThreeThings/ThreeThings.js
+++ client/src/components/ThreeThings/ThreeThings.js
@@ -8,11 +8,12 @@ const ThreeThings = props => (
     <div class="item">
       <div class="middle aligned content">
         <h1 class="ui header">
-          First featurette heading. <span class="disabled">It'll blow your mind.</span>
+        Always Remember 
+        {/* <span class="disabled">It'll blow your mind.</span> */}
         </h1>
         <div class="description">
           <p>
-            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+            A revolutionary way to remember loved ones and friends
           </p>
         </div>
       </div>
@@ -26,11 +27,14 @@ const ThreeThings = props => (
       </div>
       <div class="middle aligned content">
         <h1 class="ui header">
-          Oh yeah, <span class="disabled">it's that good.</span>
+        Keep memories alive forever 
+         {/* <span class="disabled">it's that good.</span> */}
         </h1>
         <div class="description">
           <p>
-            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+          Share precious moments with friends and loved ones
+	Through an easy to use interface
+
           </p>
         </div>
       </div>
@@ -38,11 +42,12 @@ const ThreeThings = props => (
     <div class="item">
       <div class="middle aligned content">
         <h1 class="ui header">
-          And lastly, <span class="disabled">this one.</span>
+        Build a connection web 
+          {/* <span class="disabled">this one.</span> */}
         </h1>
         <div class="description">
           <p>
-            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+          Through the process of connecting with loved ones and friends you build connections which can expand your circle of memories
           </p>
         </div>
       </div>
--- client/src/pages/Browsepage/Browsepage.js
+++ client/src/pages/Browsepage/Browsepage.js
@@ -4,14 +4,21 @@ import Search from "../../components/Search";
 import Browse from "../../components/Browse";
 
 class Browsepage extends Component {
+    
     render() {
         return (
-            <div>
-                <Nav/> 
-                <div className="search-div">
+            
+            <div style={{background:"url(https://image.ibb.co/h1S0OK/pablo_7.png)", 
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"}}>
+                <div className="nav-browse" style= {{background:"black"}}>
+                {/* <Nav />  */}
+                </div>
+                <div className="search-div"  style={{padding:"400px 0px 0px 0px", marginBottom:"0px"}}>
                 <Search/>
                 </div>
-                <h1>Reconnect with loved ones</h1>
+                <h1 style={{color:"white",  textAlign:"center"}}>Reconnect with loved ones</h1>
+                
                 <Browse/>
             </div>
 
