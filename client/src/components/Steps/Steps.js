9cd33f5183a36181c8755c9e6ac43002ed5bc657
Ken <khuynh024@gmail.com>
2018-08-23 19:12:12 -0500

    nothing

--- client/src/components/Steps/Steps.js
+++ client/src/components/Steps/Steps.js
@@ -6,26 +6,27 @@ const Steps = props => (
   <div class="column">
     <img class="ui centered small circular image" src="http://www.txstate.edu/cachee573b69ce9465908a27e25de0df62a92/imagehandler/scaler/gato-docs.its.txstate.edu/jcr:bb4d517e-7d5c-41fb-866b-b547ba49dfbc/Step%2B1.png?mode=fit&amp;width=234" />
     <h1 class="ui header">
-      Create an account
+      Create an Acount
     </h1>
-    <p></p>
-    Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+    <p>
+      Signing up is simple and easy. <br></br>Click on the Sign Up link and you'll be on your way!
+    </p>
 
   </div>
   <div class="column">
     <img class="ui centered small circular image" src="http://www.txstate.edu/cachee573b69ce9465908a27e25de0df62a92/imagehandler/scaler/gato-docs.its.txstate.edu/jcr:b633724f-ac00-443f-aa4c-6248cd890cb7/Step%2B2.png?mode=fit&amp;width=234" />
     <h1 class="ui header">
-      Share memories
+      Design a Tribute
     </h1>
     <p>
-      Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
+      
     </p>
 
   </div>
   <div class="column">
     <img class="ui centered small circular image" src="http://www.txstate.edu/cachefcf9bbef0bef7cdca4662ec7ae8cb39c/imagehandler/scaler/gato-docs.its.txstate.edu/jcr:4f124a77-07a3-4ebc-8b8e-89bc8a7a76c3/Step%2B3.png?mode=fit&amp;width=234" />
     <h1 class="ui header">
-      another thing
+      Memorialize Forever
     </h1>
     <p>
       Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
