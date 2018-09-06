123e75205d7451d024f9e22e39f06dae38e7fff4
Nick Akrap <nakrap@gmail.com>
2018-09-06 15:54:21 -0500

    added scroll to bio, and removed border

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import isEmpty from '../../validation/is-empty';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
     const { profile } = this.props;
 
     // get first name
-    const firstName = profile.name.trim().split(' ')[0];
+    const firstName = profile.name.trim().split(" ")[0];
 
     return (
       <div className="row">
         <div className="col-md-12">
-          <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">{firstName}'s bio</h3>
+          <div className="card card-body bg-light mb-3" id="about-tribute">
+            <h3 className="text-center text-info">
+              {firstName}
+              's bio
+            </h3>
             <h6 className="text-center text-info">Birthday: {profile.DOB}</h6>
-            <p id='bio-description' className="lead">{isEmpty(profile.bio) ? null : (<span>{profile.bio}</span>)} 
+            <p id="bio-description" className="lead">
+              {isEmpty(profile.bio) ? null : <span>{profile.bio}</span>}
             </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
