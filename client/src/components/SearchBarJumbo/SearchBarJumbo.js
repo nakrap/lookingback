6c173bd0ff8523b4dc99f31434622d74d96f4624
Nick Akrap <nakrap@gmail.com>
2018-09-06 14:16:28 -0500

    tinkering

--- client/src/components/SearchBarJumbo/SearchBarJumbo.js
+++ client/src/components/SearchBarJumbo/SearchBarJumbo.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./SearchBarJumbo.css";
-import logo2 from "./lookBackLogo600x600.png";
-
+// import logo2 from "./lookBackLogo600x600.png";
 
 const Search = () => (
-    <div class="ui search">
-        {/* <img id='lbLogo' className="center"  src={logo2}/>   */}
+  <div class="ui search">
+    {/* <img id='lbLogo' className="center"  src={logo2}/>   */}
 
-        <div class="ui input search">
-            <input id="search-area" type="text" placeholder="search"></input>
-            <button id="search" class="ui button search-btn">Search</button>
-            <div class="results"></div>
-        </div>
+    <div class="ui input search">
+      <input id="search-area" type="text" placeholder="search" />
+      <button id="search" class="ui button search-btn">
+        Search
+      </button>
+      <div class="results" />
     </div>
-)
+  </div>
+);
 
 export default Search;
